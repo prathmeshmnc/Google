284bdccc14f90a82a96b553fef1bf0a17615772b
badaveamruta <59682482+badaveamruta@users.noreply.github.com>
2022-11-15 14:38:22 +0530

    My first commit

---
+++ src/Testng/Emailable.java
@@ -0,0 +1,21 @@
+package Testng;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Emailable {
+@Test
+public void TC1() {
+	Reporter.log("running tc1",true);
+}
+@Test
+public void TC2() {
+	Reporter.log("running tc2",true);
+}
+@Test
+public void TC3() {
+	Reporter.log("running tc2",true);
+	Assert.fail();
+}
+}
---
+++ src/Testng/Test_ng.java
@@ -0,0 +1,11 @@
+package Testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test_ng {
+@Test
+public void m1() {
+	Reporter.log("Hi",true);
+}
+}
